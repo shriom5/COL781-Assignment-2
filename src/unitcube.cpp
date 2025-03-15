e6ecf9497d29870ddaad9fdeb996bb732778e6b4
shriom5 <ee1210642@iitd.ac.in>
2025-03-15 14:17:00 +0530

    cube done

--- src/unitcube.cpp
+++ src/unitcube.cpp
@@ -73,36 +73,191 @@ Mesh unitCube(int m, int n, int o)
 
     //bottom face
     int sz=vertices.size();
-    vec3 position = vec3(0.5,0.5,0.0);
+    vec3 position = vec3(1.0/(float)m,1.0/(float)n,0.0);
     for(int i=0;i<n-1;i++)
     {
         for(int j=0;j<m-1;j++)
         {
+            // std::cout<<position.x<<" "<<position.y<<" "<<position.z<<std::endl;
             vertices.emplace_back(MeshVertex(nullptr,position));
             position.x+=1.0/(float)m;
         }
         position.y+=1.0/(float)n;
+        position.x=1.0/(float)m;
     }
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<m;j++)
         {
             int l1,l2,l3,l4;
-            if(i==0)
+            //l1
+            if(i==0) l1=j;
+            else if (j==0) l1=2*m+2*n-i;
+            else l1=sz+(i-1)*(m-1)+j-1;
+            //l2
+            if(i==0) l2=j+1;
+            else if (j==m-1) l2=m+i;
+            else l2=sz+(i-1)*(m-1)+j;
+            //l3
+            if(i==n-1) l3=2*m+n-j-1;
+            else if (j==m-1) l3=m+i+1;
+            else l3=sz+i*(m-1)+j;
+            //l4
+            if(i==n-1) l4=2*m+n-j;
+            else if (j==0) l4=2*m+2*n-i-1;
+            else l4=sz+i*(m-1)+j-1;
+
+            if(l1<0 || l2<0 || l3<0 || l4<0) std::cout<<l1<<" "<<l2<<" "<<l3<<" "<<l4<<std::endl;
+            if(l1>=vertices.size() || l2>=vertices.size() || l3>=vertices.size() || l4>=vertices.size()) std::cout<<l1<<" "<<l2<<" "<<l3<<" "<<l4<<std::endl;
+
+            HalfEdge *e1 = new HalfEdge(l1);
+            HalfEdge *e2 = new HalfEdge(l2);
+            HalfEdge *e3 = new HalfEdge(l3);
+            HalfEdge *e4 = new HalfEdge(l4);
+            e1->next=e2;
+            e2->next=e3;
+            e3->next=e4;
+            e4->next=e1;
+            MeshFace currFace(e1,vec3(0.0,0.0,-1.0));
+            halfEdges.emplace_back(e1);
+            halfEdges.emplace_back(e2);
+            halfEdges.emplace_back(e3);
+            halfEdges.emplace_back(e4);
+            faces.emplace_back(currFace);
+            if(l1>=sz) vertices[l1].edge=e1;
+            if(l2>=sz) vertices[l2].edge=e2;
+            if(l3>=sz) vertices[l3].edge=e3;
+            if(l4>=sz) vertices[l4].edge=e4;
+
+            int currFaceIndex = faces.size()-1;
+            if(j>0)
             {
-                l1=j;l2=j+1;
+                faces[currFaceIndex-1].edge->next->twin=e4;
+                e4->twin=faces[currFaceIndex-1].edge->next;
             }
-            else if (i==n-1)
+            if (j==0)
             {
-                l3=2*m+n-j-1;l4=2*m+n-j;
+                // if(faces[2*m+2*n-i-1].edge->twin) std::cout<<"error"<<std::endl;
+                faces[2*m+2*n-i-1].edge->twin=e4;
+                e4->twin=faces[2*m+2*n-i-1].edge;
             }
-            else
+            if (j==m-1)
             {
-                l1=sz+(i-1)*m+j;
+                faces[m+i].edge->twin=e2;
+                e2->twin=faces[m+i].edge;
+            }
+            if(i>0)
+            {
+                faces[currFaceIndex-m].edge->next->next->twin=e1;
+                e1->twin=faces[currFaceIndex-m].edge->next->next;
+            }
+            if (i==0)
+            {
+                faces[j].edge->twin=e1;
+                e1->twin=faces[j].edge;
+            }
+            if (i==n-1)
+            {
+                faces[2*m+n-j-1].edge->twin=e3;
+                e3->twin=faces[2*m+n-j-1].edge;
             }
         }
     }
 
+    //top face
+    sz=vertices.size();
+    position = vec3(1.0/(float)m,1.0/(float)n,1.0);
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<m-1;j++)
+        {
+            // std::cout<<position.x<<" "<<position.y<<" "<<position.z<<std::endl;
+            vertices.emplace_back(MeshVertex(nullptr,position));
+            position.x+=1.0/(float)m;
+        }
+        position.y+=1.0/(float)n;
+        position.x=1.0/(float)m;
+    }
+    int offset=o*(2*m+2*n), face_offset=(o-1)*(2*m+2*n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            int l1,l2,l3,l4;
+            //l1
+            if(i==0) l1=offset+j;
+            else if (j==0) l1=offset+2*m+2*n-i;
+            else l1=sz+(i-1)*(m-1)+j-1;
+            //l2
+            if(i==0) l2=offset+j+1;
+            else if (j==m-1) l2=offset+m+i;
+            else l2=sz+(i-1)*(m-1)+j;
+            //l3
+            if(i==n-1) l3=offset+2*m+n-j-1;
+            else if (j==m-1) l3=offset+m+i+1;
+            else l3=sz+i*(m-1)+j;
+            //l4
+            if(i==n-1) l4=offset+2*m+n-j;
+            else if (j==0) l4=offset+2*m+2*n-i-1;
+            else l4=sz+i*(m-1)+j-1;
+
+            if(l1<0 || l2<0 || l3<0 || l4<0) std::cout<<l1<<" "<<l2<<" "<<l3<<" "<<l4<<std::endl;
+            if(l1>=vertices.size() || l2>=vertices.size() || l3>=vertices.size() || l4>=vertices.size()) std::cout<<l1<<" "<<l2<<" "<<l3<<" "<<l4<<std::endl;
+
+            HalfEdge *e1 = new HalfEdge(l1);
+            HalfEdge *e2 = new HalfEdge(l2);
+            HalfEdge *e3 = new HalfEdge(l3);
+            HalfEdge *e4 = new HalfEdge(l4);
+            e1->next=e2;
+            e2->next=e3;
+            e3->next=e4;
+            e4->next=e1;
+            MeshFace currFace(e1,vec3(0.0,0.0,1.0));
+            halfEdges.emplace_back(e1);
+            halfEdges.emplace_back(e2);
+            halfEdges.emplace_back(e3);
+            halfEdges.emplace_back(e4);
+            faces.emplace_back(currFace);
+            if(l1>=sz) vertices[l1].edge=e1;
+            if(l2>=sz) vertices[l2].edge=e2;
+            if(l3>=sz) vertices[l3].edge=e3;
+            if(l4>=sz) vertices[l4].edge=e4;
+
+            int currFaceIndex = faces.size()-1;
+            if(j>0)
+            {
+                faces[currFaceIndex-1].edge->next->twin=e4;
+                e4->twin=faces[currFaceIndex-1].edge->next;
+            }
+            if (j==0)
+            {
+                // if(faces[2*m+2*n-i-1].edge->twin) std::cout<<"error"<<std::endl;
+                // if(face_offset+2*m+2*n-i-1>=faces.size()) std::cout<<offset+2*m+2*n-i-1<<std::endl;
+                faces[face_offset+2*m+2*n-i-1].edge->next->next->twin=e4;
+                e4->twin=faces[face_offset+2*m+2*n-i-1].edge->next->next;
+            }
+            if (j==m-1)
+            {
+                faces[face_offset+m+i].edge->next->next->twin=e2;
+                e2->twin=faces[face_offset+m+i].edge->next->next;
+            }
+            if(i>0)
+            {
+                faces[currFaceIndex-m].edge->next->next->twin=e1;
+                e1->twin=faces[currFaceIndex-m].edge->next->next;
+            }
+            if (i==0)
+            {
+                faces[face_offset+j].edge->next->next->twin=e1;
+                e1->twin=faces[face_offset+j].edge->next->next;
+            }
+            if (i==n-1)
+            {
+                faces[face_offset+2*m+n-j-1].edge->next->next->twin=e3;
+                e3->twin=faces[face_offset+2*m+n-j-1].edge->next->next;
+            }
+        }
+    }
 
 
     return Mesh(halfEdges,vertices,faces);
@@ -110,7 +265,7 @@ Mesh unitCube(int m, int n, int o)
 
 int main() {
     //Create a unit square mesh
-    Mesh check=unitCube(1,1,1);
+    Mesh check=unitCube(10,5,10);
 
     check.triangulate();
 
